ed4ff2c6d5faf4bef2080e9063efad5eb445d9bd
rodygayz1 <131283134+rodygayz1@users.noreply.github.com>
2023-04-25 21:23:29 +0500

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -11,7 +11,7 @@ logging.basicConfig(
 
 class Config(object):
 
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5859491010:AAGem4C5p9XaqOVL5B3ixbCaug6aCnlODuM")
 
     API_ID = int(os.environ.get("API_ID", 12345))
 
